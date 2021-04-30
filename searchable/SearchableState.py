9b46b5770ee732634895f749d51edfda33d5ab05
amnon.a <amnon.a@appsflyer.com>
2021-04-30 21:55:38 +0300

    :sparkles: adding searchable class

---
+++ searchable/SearchableState.py
@@ -0,0 +1,21 @@
+import sys
+
+
+class SearchableState:
+    def __init__(self, stateValue):
+        self.stateValue = stateValue
+        self.stateCost = sys.maxint
+        self.stateFather = None
+
+    def __eq__(self, other):
+        if not isinstance(other, SearchableState):
+            return False
+        return self.stateValue == other.stateValue
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def compareTo(self, other):
+        if not isinstance(other, SearchableState):
+            return False
+        return self.stateCost - other.stateCost
---
+++ searchable/SearchableXPuzzle.py
@@ -0,0 +1,70 @@
+from searchable.SearchableState import SearchableState
+
+
+# search for the empty place in the state
+def searchFor0Pos(state, rowAndColAmount):
+    for row in range(rowAndColAmount):
+        for col in range(rowAndColAmount):
+            if state[row][col].stateValue == 0:
+                return row, col
+
+
+# generate a new state
+def generateState(currState, zeroRow, zeroCol, swapRow, swapCol):
+    temp = currState.stateValue.clone()
+    temp[zeroRow][zeroCol] = temp[swapRow][swapCol]
+    temp[swapRow][swapCol] = 0
+    return SearchableState(temp)
+
+
+class SearchableXPuzzle:
+
+    def __init__(self, startState, goalState):
+        """
+        :param startState problem start state as a matrix
+        :param goalState problem goal state as a matrix
+        """
+        self.rowAndColAmount = len(startState)
+        self.startState = SearchableState(startState)
+        self.goalState = SearchableState(startState)
+
+    def getAllPossibleStates(self, state):
+        """
+        used to receive all relevant possible states.
+
+        :param state: state to create neighbors to
+        :return: array of neighbors states
+        """
+        neighbors = []
+        row, col = searchFor0Pos(state, self.rowAndColAmount)
+
+        if row != 0:
+            newState = generateState(state, row, col, row - 1, col)
+            newState.stateCost = state.stateCost + 1
+            neighbors.append(newState)
+
+        if row != self.rowAndColAmount - 1:
+            newState = generateState(state, row, col, row + 1, col)
+            newState.stateCost = state.stateCost + 1
+            neighbors.append(newState)
+
+        if col != 0:
+            newState = generateState(state, row, col, row, col - 1)
+            newState.stateCost = state.stateCost + 1
+            neighbors.append(newState)
+
+        if col != self.rowAndColAmount - 1:
+            newState = generateState(state, row, col, row, col + 1)
+            newState.stateCost = state.stateCost + 1
+            neighbors.append(newState)
+
+        return neighbors
+
+    def isGoalState(self, state):
+        """
+        used to check if current state is the goal state
+
+        :param state: state to check
+        :return: state is goal state ? true : false
+        """
+        return state == self.goalState
