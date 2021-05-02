ad65dbf1b4229eb33f2f1a6604d8bb42476fd60e
omer <ordt97@gmail.com>
2021-05-02 22:25:00 +0300

    add Astar algo

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from searchAlgorithms.BFS import BFS
 from searchable.SearchableXPuzzle import SearchableXPuzzle
 from searchAlgorithms.IDS import IDS
 
-
+# CR: In python we use snakeCase and not camelCase
+# like : def generate_goal_state(matrix_length):
+# need to change all the files :\
 def generateGoalState(matrixLength):
     matrix = []
     for row in range(matrixLength):
@@ -34,10 +36,13 @@ def main():
     goalState = generateGoalState(size)
     problem = SearchableXPuzzle(startState, goalState)
 
+    # CR : Why not switch case?
     if algo == 1:
         routeToGoal = IDS(problem)
+        #CR : in python3, print function has been replaced by the print() function, meaning that we have to wrap the object that we want to print in parantheses.
         print routeToGoal
     elif algo == 2:
+        # CR : same as above
         routeToGoal = BFS(problem)
         print routeToGoal
     elif algo == 3:
---
+++ searchAlgorithms/AStar.py
@@ -0,0 +1,62 @@
+from searchable.SearchableXPuzzle import SearchableXPuzzle
+
+
+def best_fvalue(o_list):
+    f = o_list[0].f
+    index = 0
+    for i, item in enumerate(o_list):
+        if i == 0:
+            continue
+        if item.f < f:
+            f = item.f
+            index = i
+
+    return o_list[index], index
+
+
+def a_star(problem):
+    """
+    Used to find goal state using the AStar algorithm
+
+    :param problem: x puzzle problem
+    :return: route to goal state
+    """
+    if not isinstance(problem, SearchableXPuzzle):
+        raise Exception('problem must be instance of SearchableXPuzzle')
+
+    o_list, c_list = [], []
+    init_state = problem.startState
+    o_list.append(init_state)
+    while o_list:
+        current, index = best_fvalue(o_list)
+        if problem.isGoalState(current):
+            return problem.getRoute(current)
+        o_list.pop(index)
+        c_list.append(current)
+
+        for move in problem.getAllPossibleStates(current):
+            ok = False  # checking in closedList
+            for i, item in enumerate(c_list):
+                if item == move:
+                    ok = True
+                    break
+            if not ok:  # not in closed list
+                new_g = current.g + 1
+                present = False
+
+                # openList includes move
+                for j, item in enumerate(o_list):
+                    if item == move:
+                        present = True
+                        if new_g < o_list[j].g:
+                            o_list[j].g = new_g
+                            o_list[j].f = o_list[j].g + o_list[j].h
+                            o_list[j].parent = current
+                if not present:
+                    move.g = new_g
+                    move.h = move.manhattan()
+                    move.f = move.g + move.h
+                    move.parent = current
+                    o_list.append(move)
+
+        return None
