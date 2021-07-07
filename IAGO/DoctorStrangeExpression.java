94b5d9ebe4d9abf44aa1f1c3fae114660070361b
omer <ordt97@gmail.com>
2021-07-07 22:04:08 +0300

    Changing messages and expression - phase 1

--- IAGO/DoctorStrangeExpression.java
+++ IAGO/DoctorStrangeExpression.java
@@ -4,94 +4,66 @@ import edu.usc.ict.iago.utils.Event;
 import edu.usc.ict.iago.utils.ExpressionPolicy;
 import edu.usc.ict.iago.utils.History;
 
-public class RepeatedFavorExpression extends IAGOCoreExpression implements ExpressionPolicy{
-	
-	protected String getSemiFairEmotion()
-	{
-		return "happy";
-	}
-	
-	protected String getFairEmotion()
-	{
-		return "happy";
-	}
-	
-	protected String getUnfairEmotion()
-	{
-		return "sad";
-	}
+public class DoctorStrangeExpression extends IAGOCoreExpression implements ExpressionPolicy {
 
-	@Override
-	public String getExpression(History history) 
-	{
-		Event last = history.getUserHistory().getLast();
-		if(last.getType().equals(Event.EventClass.SEND_EXPRESSION)){
-			if(last.getMessage().equals("sad") || last.getMessage().equals("neutral"))
-				return "sad";
-			else if(last.getMessage().equals("happy"))
-				return "happy";
-			else if(last.getMessage().equals("surprised"))
-				return "neutral";
-			else if(last.getMessage().equals("angry"))
-				return "sad";
-			else
-				return null;
-		} 
-		else if (last.getType().equals(Event.EventClass.SEND_MESSAGE))
-		{
-			Event.SubClass type = last.getSubClass();
-			switch(type)
-			{
-				case BATNA_INFO:
-					return "happy";
-				case BATNA_REQUEST:
-					return null;
-				case CONFUSION:
-					return "sad";
-				case FAVOR_ACCEPT:
-					return "happy";
-				case FAVOR_REJECT:
-					return "sad";
-				case FAVOR_REQUEST:
-					return null;
-				case FAVOR_RETURN:
-					return "happy";
-				case GENERIC_NEG:
-					return "sad";
-				case GENERIC_POS:
-					return "happy";
-				case NONE:
-					return null;
-				case OFFER_ACCEPT:
-					return "happy";
-				case OFFER_REJECT:
-					return "sad";
-				case OFFER_REQUEST_NEG:
-					return "sad";
-				case OFFER_REQUEST_POS:
-					return null;
-				case PREF_INFO:
-					return "happy";
-				case PREF_REQUEST:
-					return null;
-				case PREF_SPECIFIC_REQUEST:
-					return null;
-				case PREF_WITHHOLD:
-					return "sad";
-				case THREAT_NEG:
-					return "sad";
-				case THREAT_POS:
-					return "sad";
-				case OFFER_PROPOSE:
-					return null;
-				case TIMING:
-					return null;
-				default:
-					return null;			
-			}
-		}
-		else
-			return null;
-		
-	}
+    protected String getSemiFairEmotion() {
+        return "neutral";
+    }
+
+    protected String getFairEmotion() {
+        return "happy";
+    }
+
+    protected String getUnfairEmotion() {
+        return "angry";
+    }
+
+    @Override
+    public String getExpression(History history) {
+        Event userEvent = history.getUserHistory().getLast();
+        if (userEvent.getType().equals(Event.EventClass.SEND_MESSAGE)) {
+            Event.SubClass type = userEvent.getSubClass();
+            switch (type) {
+                case BATNA_INFO:
+                    return "happy";
+                case CONFUSION:
+                    return "sad";
+                case FAVOR_ACCEPT:
+                    return "happy";
+                case FAVOR_REJECT:
+                    return "angry";
+                case FAVOR_REQUEST:
+                    return "neutral";
+                case FAVOR_RETURN:
+                    return "happy";
+                case GENERIC_NEG:
+                    return "sad";
+                case GENERIC_POS:
+                    return "surprised";
+                case OFFER_ACCEPT:
+                    return "happy";
+                case OFFER_REJECT:
+                    return "angry";
+                case OFFER_REQUEST_NEG:
+                    return "sad";
+                case OFFER_REQUEST_POS:
+                    return "happy";
+                case PREF_INFO:
+                    return "happy";
+                case PREF_WITHHOLD:
+                    return "neutral";
+                case THREAT_NEG:
+                    return "sad";
+                case THREAT_POS:
+                    return "neutral";
+                default:
+                    return null;
+            }
+        } else if (userEvent.getType().equals(Event.EventClass.FORMAL_ACCEPT) {
+            return "happy";
+        }
+        else{
+            return null;
+        }
+    }
 }
--- IAGO/DoctorStrangeIAGOAgent.java
+++ IAGO/DoctorStrangeIAGOAgent.java
@@ -6,15 +6,15 @@ import edu.usc.ict.iago.utils.GameSpec;
 
 public class DoctorStrangeIAGOAgent extends IAGOCoreVH {
 
-	// Constructor
-	public DoctorStrangeIAGOAgent(String name, GameSpec game, Session session) {
-		super("DoctorStrangeIAGOAgent", game, session, new DoctorStrangeBehavior(), /* expression */, /* message */);
-		super.safeForMultiAgent = true;
-	}
+    // Constructor
+    public DoctorStrangeIAGOAgent(String name, GameSpec game, Session session) {
+        super("DoctorStrangeIAGOAgent", game, session, new DoctorStrangeBehavior(), new DoctorStrangeExpression(), new DoctorStrangeMessage(game));
+        super.safeForMultiAgent = true;
+    }
 
-	@Override
-	public String getArtName() { return "DoctorStrangeIAGOAgent"; }
+    @Override
+    public String getArtName() { return "DoctorStrangeIAGOAgent"; }
 
-	@Override
-	public String agentDescription() { return "<h1>Doctor Strange</h1><p> is excited to begin negotiating!</p>"; }
+    @Override
+    public String agentDescription() { return "<h1>Doctor Strange</h1><p> is excited to begin negotiating!</p>"; }
 }
